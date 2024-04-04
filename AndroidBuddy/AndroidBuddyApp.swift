3d61907b302d968d5eef2be75d8044821a088dd0
Mark Borazio <markborazio@hotmail.com>
2024-04-04 19:32:23 +1100

    Project setup

---
+++ AndroidBuddy/AndroidBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AndroidBuddyApp.swift
+//  AndroidBuddy
+//
+//  Created by Mark Borazio [Personal] on 1/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct AndroidBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
