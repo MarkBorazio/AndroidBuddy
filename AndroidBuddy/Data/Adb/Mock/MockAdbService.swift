966b9f047d7afb8426f10592890e8acf4711c223
Mark Borazio <markborazio@hotmail.com>
2024-04-22 04:42:08 +1000

    Make mock ADB service more mockable.

--- AndroidBuddy/Data/Adb/Mock/MockAdbService.swift
+++ AndroidBuddy/Data/Adb/Mock/MockAdbService.swift
@@ -8,43 +8,119 @@
 import Foundation
 import Combine
 
-class MockAdbService: ADBService {
+class MockAdbService {
     
     var connectedDevices: any Publisher<[Device], Error>
     var state: any Publisher<ADBServiceState, Never>
     
-    init(adbState: ADBServiceState, devices: [Device]) {
+    var startServerBlock: () -> Void
+    var killServerBlock: () -> Void
+    var listBlock: (URL) -> ListCommandResponse
+    var pullBlock: () -> any Publisher<PullProgressResponse, Error>
+    var pushBlock: () -> Void
+    var deleteBlock: () -> Void
+    var getBluetoothNameBlock: () -> String
+    var devicesBlock: () -> DevicesResponse
+    
+    init(
+        adbState: ADBServiceState,
+        devices: [Device],
+        startServerBlock: @escaping () -> Void = defaultStartServerBlock,
+        killServerBlock: @escaping () -> Void = defaultKillServerBlock,
+        listBlock: @escaping (URL) -> ListCommandResponse = defaultListBlock,
+        pullBlock: @escaping () -> any Publisher<PullProgressResponse, Error> = defaultPullBlock,
+        pushBlock: @escaping () -> Void = defaultPushBlock,
+        deleteBlock: @escaping () -> Void = defaultDeleteBlock,
+        getBluetoothNameBlock: @escaping () -> String = defaultGetBluetoothNameBlock,
+        devicesBlock: @escaping () -> DevicesResponse = defaultDevicesBlock
+    ) {
         connectedDevices = CurrentValueSubject(devices).eraseToAnyPublisher()
         state = CurrentValueSubject(adbState).eraseToAnyPublisher()
+        
+        self.startServerBlock = startServerBlock
+        self.killServerBlock = killServerBlock
+        self.listBlock = listBlock
+        self.pullBlock = pullBlock
+        self.pushBlock = pushBlock
+        self.deleteBlock = deleteBlock
+        self.getBluetoothNameBlock = getBluetoothNameBlock
+        self.devicesBlock = devicesBlock
     }
     
-    func startServer() {}
-    
-    func killServer() async throws {}
-    
-    func list(serial: String, path: URL) async throws -> ListCommandResponse {
-        let response = getResponse(fileName: "MockListResponse")
-        return ListCommandResponse(path: path, rawResponse: response)
-    }
-    
-    func pull(serial: String, remotePath: URL) -> any Publisher<PullProgressResponse, Error> { Empty() }
-    
-    func push(serial: String, localPath: URL, remotePath: URL) async throws {}
-    
-    func delete(serial: String, remotePath: URL) async throws {}
-    
-    func getBluetoothName(serial: String) async throws -> String {
-        "Mock Device Name"
-    }
-    
-    func devices() async throws -> DevicesResponse {
-        let response = getResponse(fileName: "MockDevicesResponse")
-        return DevicesResponse(rawOutput: response)
-    }
-    
-    private func getResponse(fileName: String) -> String {
+    private static func getResponse(fileName: String) -> String {
         let url = Bundle.main.url(forResource: fileName, withExtension: nil)!
         let data = try! Data(contentsOf: url)
         return String(data: data, encoding: .utf8)!
     }
+}
+
+// MARK: - Default Implementations
+
+extension MockAdbService {
+    
+    static var defaultStartServerBlock: () -> Void = {}
+    
+    static var defaultKillServerBlock: () -> Void = {}
+    
+    static var defaultListBlock: (URL) -> ListCommandResponse = { path in
+        let response = getResponse(fileName: "MockListResponse")
+        return ListCommandResponse(path: path, rawResponse: response)
+    }
+    
+    static var defaultPullBlock: () -> any Publisher<PullProgressResponse, Error> = {
+        let response = PullProgressResponse(rawOutput: "[ 39%] /sdcard/Roms/Gamecube/Super Mario Strikers.iso")
+        return CurrentValueSubject<PullProgressResponse, Error>(response)
+    }
+    
+    static var defaultPushBlock: () -> Void = {}
+    
+    static var defaultDeleteBlock: () -> Void = {}
+    
+    static var defaultGetBluetoothNameBlock: () -> String = {
+        "Mock Device Name"
+    }
+    
+    static var defaultDevicesBlock: () -> DevicesResponse = {
+        let response = getResponse(fileName: "MockDevicesResponse")
+        return DevicesResponse(rawOutput: response)
+    }
+    
+}
+
+// MARK: - ADB Service Implementation
+
+extension MockAdbService: ADBService {
+    
+    func startServer() {
+        startServerBlock()
+    }
+    
+    func killServer() async throws {
+        killServerBlock()
+    }
+    
+    func list(serial: String, path: URL) async throws -> ListCommandResponse {
+        listBlock(path)
+    }
+    
+    func pull(serial: String, remotePath: URL) -> any Publisher<PullProgressResponse, Error> {
+        pullBlock()
+    }
+    
+    func push(serial: String, localPath: URL, remotePath: URL) async throws {
+        pushBlock()
+    }
+    
+    func delete(serial: String, remotePath: URL) async throws {
+        deleteBlock()
+    }
+    
+    func getBluetoothName(serial: String) async throws -> String {
+        getBluetoothNameBlock()
+    }
+    
+    func devices() async throws -> DevicesResponse {
+        devicesBlock()
+    }
+    
 }
